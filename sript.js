7eb55930ef485c70ba51cf61be351964a3df554b
chemuu <chemutdor@gmail.com>
2026-05-06 15:40:47 +0300

    Fix JavaScript errors in booking functionality and improve event listener syntax

--- sript.js
+++ sript.js
@@ -1,20 +1,19 @@
 // Button click
-document.querySelector("book-btn").addEventListener("click",function(){
+document.querySelector(".book-btn").addEventListener("click",function(){
     alert("Booking feature coming soon!");
-})
+});
 
 // Form Validation
-document.querySelector(".booking-form").addEventListener("submit",function(event{
+document.querySelector(".booking-form").addEventListener("submit",function(event){
     let name=document.querySelector(".name").value;
     let date=document.querySelector(".date").value;
 
     if(name ===""|| date ===""){
         alert("please fill in all fields!");
         event.preventDefault();
-    }else{
+    } else {
         alert("Appointment booked succefully!");
     }
-
 });
 
 // image click effect
@@ -22,7 +21,7 @@ let images= document.querySelectorAll(".salon-image");
 
 images.forEach(function(img){
     img.addEventListener("click",function(){
-        this style.transform = "scale(1.2");
-        this.style.transiyion = "0.3s";
+        this.style.transform = "scale(1.2)";
+        this.style.transition = "0.3s";
     });
 });
