075499308372ddc5e31d932f045bb50098c3aee8
chemuu <chemutdor@gmail.com>
2026-05-07 22:19:15 +0300

    Enhance booking form with additional fields for date and improved validation; update local storage handling for booking data

--- sript.js
+++ sript.js
@@ -1,15 +1,43 @@
-// Button click
-document.querySelector(".book-btn").addEventListener("click", function () {
-  alert("Booking feature coming soon!");
+document.querySelector(".book-btn").addEventListener("click", function(){
+    alert(".Booking feature coming soon!");
 });
+// Form validation& local storage
 
-// Form Validation & local storage
-document
-  .querySelector(".booking-form")
-  .addEventListener("submit", function (event) {
+document.querySelector(".booking-form").addEventListener("submit", function (event){
+    event.preventDefault();
+
     const name = document.querySelector(".name").value;
     const date = document.querySelector(".date").value;
+    const email = document.querySelector(".email").value;
     const message = document.querySelector(".message").value;
+
+    if(name === ""|| date === ""|| email === ""){
+        alert("please fill in all fields");
+
+    } else{
+        // create booking object
+        const bookingData = {
+        customerName: name,
+        customerEmail:email,
+        customerDate:date,
+        customerMessage:message
+
+
+    };
+    // save to local storage
+    localStorage.setItem(
+        "latestBooking",
+        JSON.stringify(bookingData)
+    );
+
+    // success message
+    alert("Appointment booked successfully!");
+
+}
+
+    
+
+});
 
     if (name === "" || date === "") {
       alert("please fill in all fields!");
@@ -30,6 +58,7 @@ document
 
       alert("Appointment booked succefully!");
     }
+    
   });
 
 // retrive data when the page loads
@@ -41,12 +70,3 @@ window.onload = function(){
     }
 }
 
-// image click effect
-let images = document.querySelectorAll(".salon-image");
-
-images.forEach(function (img) {
-  img.addEventListener("click", function () {
-    this.style.transform = "scale(1.2)";
-    this.style.transition = "0.3s";
-  });
-});
